0e11675c48e597ed0b383f7b2a0c2ae5cf5d035b
ThatGuyJamal <jamallyons8@gmail.com>
2023-03-03 16:35:45 -0500

    Update publish.cjs

--- publish.cjs
+++ publish.cjs
@@ -1,10 +1,14 @@
 const { publish } = require("gh-pages");
 
 publish(
-	"build",
+	"./build",
 	{
-		branch: "production",
-		remote: "origin",
+		branch: "site",
+		message: "Updated site",
+		user: {
+			name: process.env.GH_NAME,
+			email: process.env.GH_EMAIL,
+		}
 	},
 	function (err) {
 		if (err) {
